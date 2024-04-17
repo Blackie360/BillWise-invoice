c77e98a17acf59bb533ae049031f782b8ec213a4
Blackie360 <felixkent360@gmail.com>
2024-04-17 14:56:30 +0300

    conditional rendering preview and edit forms

--- app/invoice/new/page.tsx
+++ app/invoice/new/page.tsx
@@ -1,23 +1,39 @@
+"use client";
 
-import Navbar from "@/components/Navbar";
+import { FaEdit } from "react-icons/fa";
 import { FaDownload } from "react-icons/fa6";
 import { FaPrint } from "react-icons/fa6";
 import { MdPreview } from "react-icons/md";
 import { IoSaveSharp } from "react-icons/io5";
 import { MdOutlineSend } from "react-icons/md";
+import { useState } from "react";
 
 
 const New = () => {
+  const [preview, setPreview] = useState(false)
   return (
     <div className='bg-slate-50 text-black py-8'>
-      <Navbar />
   {/* header */}
-  <div className="flex justify-between items-center  md:py-8 px-4 md:px-16">
+  <div className="flex justify-between items-center  md:py-8 px-4 md:px-16 mb-6">
   <div className="flex gap-4">
    
-   <button className="flex items-center space-x-2 px-3 py-2 shadow rounded-sm border border-slate-600 ">
-       <MdPreview />
+   <button  onClick={()=>setPreview(!preview)}
+   className=" px-3 py-2 shadow rounded-sm border border-slate-600 ">
+       {
+        preview ? (
+          <div className="flex items-center space-x-2">
+            <FaEdit />
+
+       <span>Edit form</span>
+          </div>
+        ) : (
+          <div className="flex items-center space-x-2">
+            <MdPreview />
        <span>Preview</span>
+          </div>
+        )
+       }
+
        </button>
    
     <button className="flex items-center space-x-2 px-3 py-2 shadow rounded-sm border border-slate-600 ">
@@ -47,20 +63,22 @@ const New = () => {
    </div>
   </div>
   {/* invoice form */}
-  <div className=''>
-
-
-<div className="w-full max-w-sm p-4 bg-white border border-gray-600 rounded-lg shadow sm:p-6 md:p-8">
-    <form className="space-y-6" action="#">
-       
-    </form>
+  {
+    preview ? (
+        
+<div className="w-full max-w-4xl p-4 bg-white border border-gray-600 rounded-lg shadow sm:p-6 md:p-8 mx-auto">
+   <h2>preview</h2>
 </div>
-
-
-  </div>
+    ) : (
+      <div className="w-full max-w-4xl p-4 bg-white border border-gray-600 rounded-lg shadow sm:p-6 md:p-8 mx-auto">
+   <h1>form</h1>
+</div>
+    )
+  }
   {/* preview */}
 
-      <h1 className='text-black'>Creat New Invoice</h1>
+  
+
     </div>
   )
 }
