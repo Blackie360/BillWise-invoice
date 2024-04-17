b2b4d8595bfb7be17d6821f81ec241ffced27db6
Blackie360 <felixkent360@gmail.com>
2024-04-17 15:28:14 +0300

    logo input field and invoice label

--- app/invoice/new/page.tsx
+++ app/invoice/new/page.tsx
@@ -7,6 +7,9 @@ import { MdPreview } from "react-icons/md";
 import { IoSaveSharp } from "react-icons/io5";
 import { MdOutlineSend } from "react-icons/md";
 import { useState } from "react";
+import FormPreview from "@/components/FormPreview";
+import { FaCloudUploadAlt } from "react-icons/fa";
+
 
 
 const New = () => {
@@ -65,17 +68,33 @@ const New = () => {
   {/* invoice form */}
   {
     preview ? (
-        
-<div className="w-full max-w-4xl p-4 bg-white border border-gray-600 rounded-lg shadow sm:p-6 md:p-8 mx-auto">
-   <h2>preview</h2>
-</div>
+        <FormPreview />
+
     ) : (
-      <div className="w-full max-w-4xl p-4 bg-white border border-gray-600 rounded-lg shadow sm:p-6 md:p-8 mx-auto">
-   <h1>form</h1>
-</div>
+      <form className="w-full max-w-4xl p-4 bg-white border border-gray-600 rounded-lg shadow sm:p-6 md:p-8 mx-auto">
+  {/* image & lebal */}
+  <div className="flex justify-between items-center">
+    {/* image */}
+
+<div className="flex items-center justify-center ">
+    <label for="dropzone-file" className="flex flex-col items-center justify-center w-48 h-24 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 hover:bg-gray-100 ">
+        <div className="flex flex-col items-center justify-center pt-5 pb-6">
+        <FaCloudUploadAlt className="w-6 h-6  text-gray-500" />
+            <p className="mb-2 text-sm text-gray-500 dark:text-gray-400"><span className="font-semibold">upload Logo</span></p>
+            <p className="text-xs text-gray-500 dark:text-gray-400">PNG or JPG (MAX. 240x240px)</p>
+        </div>
+        <input id="dropzone-file" type="file" className="hidden" />
+    </label>
+</div> 
+
+
+
+    <h2 className="text-4xl uppercase font-bold">Invoice</h2>
+  </div>
+</form>
     )
   }
-  {/* preview */}
+ 
 
   
 
---
+++ components/FormPreview.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const FormPreview = () => {
+  return (
+    <div className="w-full max-w-4xl p-4 bg-white border border-gray-600 rounded-lg shadow sm:p-6 md:p-8 mx-auto">
+   <h2>preview</h2>
+</div>
+  )
+}
+
+export default FormPreview
