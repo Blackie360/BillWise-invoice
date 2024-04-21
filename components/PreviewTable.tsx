72690667e17fe7499ed4bce00b8f437dee62b5a1
Felix Jumason <felixkent360@gmail.com>
2024-04-22 01:49:58 +0300

    removed dark theme

--- components/PreviewTable.tsx
+++ components/PreviewTable.tsx
@@ -2,79 +2,76 @@ import React from 'react'
 
 const PreviewTable = () => {
   return (
-    
-
-<div className="relative overflow-x-auto">
-    <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
-    <thead className="text-xs text-black uppercase bg-gray-50 ">
-            <tr>
-                <th scope="col" className="px-6 py-3">
-                Item Description
-                </th>
-                <th scope="col" className="px-6 py-3">
-                Quantity
-                </th>
-                <th scope="col" className="px-6 py-3">
-                Unit Price
-                </th>
-                <th scope="col" className="px-6 py-3">
-                    Tax
-                </th>
-                <th scope="col" className="px-6 py-3">
-                    Amount
-                </th>
-                <th scope="col" className="px-6 py-3">
-                    <span className="sr-only">Edit</span>
-                </th>
-            </tr>
+    <div className="relative overflow-x-auto">
+      <table className="w-full text-sm text-left text-gray-500">
+        <thead className="text-xs text-black uppercase bg-gray-50">
+          <tr>
+            <th scope="col" className="px-6 py-3">
+              Item Description
+            </th>
+            <th scope="col" className="px-6 py-3">
+              Quantity
+            </th>
+            <th scope="col" className="px-6 py-3">
+              Unit Price
+            </th>
+            <th scope="col" className="px-6 py-3">
+              Tax
+            </th>
+            <th scope="col" className="px-6 py-3">
+              Amount
+            </th>
+            <th scope="col" className="px-6 py-3">
+              <span className="sr-only">Edit</span>
+            </th>
+          </tr>
         </thead>
         <tbody>
-            <tr className="bg-white dark:bg-gray-800">
-                <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                    Apple MacBook Pro 17"
-                </th>
-                <td className="px-6 py-4">
-                    Silver
-                </td>
-                <td className="px-6 py-4">
-                    Laptop
-                </td>
-                <td className="px-6 py-4">
-                    $2999
-                </td>
-            </tr>
-            <tr className="bg-white dark:bg-gray-800">
-                <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                    Microsoft Surface Pro
-                </th>
-                <td className="px-6 py-4">
-                    White
-                </td>
-                <td className="px-6 py-4">
-                    Laptop PC
-                </td>
-                <td className="px-6 py-4">
-                    $1999
-                </td>
-            </tr>
-            <tr className="bg-white dark:bg-gray-800">
-                <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                    Magic Mouse 2
-                </th>
-                <td className="px-6 py-4">
-                    Black
-                </td>
-                <td className="px-6 py-4">
-                    Accessories
-                </td>
-                <td className="px-6 py-4">
-                    $99
-                </td>
-            </tr>
+          <tr className="bg-white">
+            <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap">
+              Apple MacBook Pro 17"
+            </th>
+            <td className="px-6 py-4">
+              Silver
+            </td>
+            <td className="px-6 py-4">
+              Laptop
+            </td>
+            <td className="px-6 py-4">
+              $2999
+            </td>
+          </tr>
+          <tr className="bg-white">
+            <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap">
+              Microsoft Surface Pro
+            </th>
+            <td className="px-6 py-4">
+              White
+            </td>
+            <td className="px-6 py-4">
+              Laptop PC
+            </td>
+            <td className="px-6 py-4">
+              $1999
+            </td>
+          </tr>
+          <tr className="bg-white">
+            <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap">
+              Magic Mouse 2
+            </th>
+            <td className="px-6 py-4">
+              Black
+            </td>
+            <td className="px-6 py-4">
+              Accessories
+            </td>
+            <td className="px-6 py-4">
+              $99
+            </td>
+          </tr>
         </tbody>
-    </table>
-</div>
-
+      </table>
+    </div>
   )
 }
 
