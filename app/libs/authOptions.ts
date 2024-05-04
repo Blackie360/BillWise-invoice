30160953659b5ebb34f14f07fffde0ae88875b95
Felix Jumason <felixkent360@gmail.com>
2024-05-04 12:58:11 +0300

    deployment

--- app/libs/authOptions.ts
+++ app/libs/authOptions.ts
@@ -4,12 +4,11 @@ import db from "./db";
 import { compare } from "bcrypt";
 
 const authOptions = {
-    adapter: PrismaAdapter(db),
-    secret: process.env.NEXTAUTH_SECRET,
-    session: {
-    strategy: "jwt",
+  adapter: PrismaAdapter(db),
+  secret: process.env.NEXTAUTH_SECRET,
+  session: {
+    jwt: true, // Use JWT sessions
   },
-
   pages: {
     signIn: "/login",
   },
@@ -21,35 +20,35 @@ const authOptions = {
         password: { label: "Password", type: "password" },
       },
       async authorize(credentials) {
-          // Check if user credentials are Correct
-          if (!credentials?.email || !credentials?.password) {
-            return null;
-          }
-          //Check if user exists
-          const existingUser = await db.user.findUnique({
-            where: { email: credentials.email },
-          });
-          if (!existingUser) {
-            return;
-          }
-          //Check if Password is correct
-          const passwordMatch = await compare(
-            credentials.password,
-            existingUser.password
-          );
-          if (!passwordMatch) {
-            return null;
-          }
-          const user= {
-            id: existingUser.id,
-            name: existingUser.name,
-            email: existingUser.email,
-          };
-          console.log(user);
-          return user;
+        // Check if user credentials are Correct
+        if (!credentials?.email || !credentials?.password) {
+          return null;
+        }
+        // Check if user exists
+        const existingUser = await db.user.findUnique({
+          where: { email: credentials.email },
+        });
+        if (!existingUser) {
+          return null;
+        }
+        // Check if Password is correct
+        const passwordMatch = await compare(
+          credentials.password,
+          existingUser.password
+        );
+        if (!passwordMatch) {
+          return null;
+        }
+        const user = {
+          id: existingUser.id,
+          name: existingUser.name,
+          email: existingUser.email,
+        };
+        console.log(user);
+        return user;
       },
     }),
   ],
 };
 
-export { authOptions };
+export default authOptions;
