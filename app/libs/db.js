8fa7d76f64c8e4a8cfe3426aac1035654a65562a
blackie360 <felixkent360@gmail.com>
2024-12-14 14:16:45 +0300

    code refactor

--- app/libs/db.js
+++
@@ -1,6 +0,0 @@
-const { PrismaClient } = require("@prisma/client");
-
-const db = globalThis.prisma || new PrismaClient();
-if (process.env.NODE_ENV !== "production") globalThis.prisma = db;
-
-export default db;
