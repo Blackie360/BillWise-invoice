a6fcbb1319c0e518892bf1f9f81016c87e6ba873
blackie360 <felixkent360@gmail.com>
2024-12-14 15:26:48 +0300

    feat:install Ui

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,10 +1,22 @@
+import { Card, CardDescription, CardHeader, CardTitle } from '@/components/ui/card'
 import React from 'react'
 
 const Login = () => {
   return (
-    <div>
-      login
-    </div>
+  <>
+  <div className="flex h-screen w-full items-center justify-center px-4">
+    <Card className='max-w-sm'>
+        <CardHeader>
+            <CardTitle className='text-2xl'>Login</CardTitle>
+            <CardDescription>
+                Enter your email below to login in to your account
+            </CardDescription>
+        </CardHeader>
+        
+    </Card>
+
+  </div>
+  </>
   )
 }
 
