aa8689e6470fb55d27f22db99149a04c6b7f8726
blackie360 <felixkent360@gmail.com>
2024-12-14 18:15:08 +0300

    feat:login ui

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,4 +1,6 @@
-import { Card, CardDescription, CardHeader, CardTitle } from '@/components/ui/card'
+import { Button } from '@/components/ui/button'
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card'
+import { Label } from '@/components/ui/label'
 import React from 'react'
 
 const Login = () => {
@@ -12,7 +14,15 @@ const Login = () => {
                 Enter your email below to login in to your account
             </CardDescription>
         </CardHeader>
-        
+        <CardContent>
+            <form className='flex flex-col gap-y-4'>
+                <div className='flex flex-col gap-y-2'>
+                    <Label>Email</Label>
+                    <input type="email" placeholder='hello@gmail.com' className='input' />
+                </div>
+                <Button>Submit</Button>
+            </form>
+        </CardContent>
     </Card>
 
   </div>
